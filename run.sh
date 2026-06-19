97abb2b26805191c1fc796a5731182ed3757670b
Ivan Andrus <darthandrus@gmail.com>
2026-06-18 23:14:50 -0600

    Notes about how to make it work on a new computer...

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
+# Log in to apple developer account
+# Accept the new PLA (there is one every few days it seems)
+# Reopen xcode
+# It will complain to register the device -- do so
+# It works... hopefully
+
 cd ~/vcs/MeetingBar/
 
 xcodebuild -scheme MeetingBar -configuration Debug \
-  -derivedDataPath ./DerivedData build
+  -derivedDataPath ./DerivedData \
+  -allowProvisioningUpdates build
 
 exec ./DerivedData/Build/Products/Debug/MeetingBar.app/Contents/MacOS/MeetingBar
